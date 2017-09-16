c66a8189df001fd2624f970d31fe01ab273ae8bf
Brian Francis <bbfrancis23@gmail.com>
2017-09-15 21:00:30 -0600

    Add Aeo Header and Dashboard component

---
+++ src/app/aeo-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {DataService} from './data.service';
+
+@Component({
+  selector: 'aeo-header',
+  templateUrl: './aeo-header.component.html',
+  styleUrls: ['./aeo-header.component.css']
+})
+export class AeoHeaderComponent {
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AeoHeaderComponent} from './aeo-header.component';
+import {DashboardComponent} from './dashboard.component';
+
 import {HttpModule} from '@angular/http';
 import {DataService} from './data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AeoHeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
