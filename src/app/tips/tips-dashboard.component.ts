2e7389f920a805254d747257b0536ee4c413837d
Brian Francis <bbfrancis23@gmail.com>
2017-09-18 10:29:16 -0600

    tips dashboard files

---
+++ src/app/tips/tips-dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+//import { IntroAreaComponent } from './intro-area.component';
+//import { ListAreaComponent } from './list-area.component';
+//import { DetailAreaComponent } from './detail-area.component';
+//import { SidebarComponent} from './sidebar.component';
+//import { LeftPaneComponent} from './left-pane.component';
+//import { RightPaneComponent} from './right-pane.component';
+import {DataService} from '../data.service';
+
+
+@Component({
+  selector: 'tips-dashboard',
+  templateUrl: './templates/tips-dashboard.component.html',
+  //styleUrls: ['./styles/dashboard.component.css']
+})
+export class DashboardComponent implements AfterViewInit {
+
+  //@ViewChild(SidebarComponent) sidebar;
+  //@ViewChild(LeftPaneComponent) leftPane;
+  //@ViewChild(RightPaneComponent) rightPane;
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit() {
+  }
+
+}
