7a0571fafb48318fbb4e2d893bb433f29c0a2a65
Brian Francis <bbfrancis23@gmail.com>
2017-09-25 14:33:07 -0600

    Update Intro Tile

--- src/app/jems/components/ts/jem-intro-tile.ts
+++ src/app/jems/components/ts/jem-intro-tile.ts
@@ -3,11 +3,17 @@ import { DashBoardTileComponent} from '../../../dash-board/components/ts/dash-bo
 
 @Component({
   selector: 'jem-intro-tile',
-  templateUrl: '../html/jem-intro-tile.html',
-  styles: [`
-
-`]
+  template:
+    ` <div class="card border-primary mt-2" *ngIf="show" >
+        <img class="card-img-top" src="assets/img/code-jems.jpg" alt="Card image cap">
+        <div class="tile-controls">
+          <a class="material-icons tile-item" (click)="show=false;" >clear</a>
+        </div>
+        <div class="card-block p-2">
+          <h4 class="card-title">Code Jems</h4>
+          <p class="card-text">Short-cut key, Best Practices, How to and Mistakes. Short Code Jems,  it's all here</p>
+        </div>
+      </div>`
 })
 export class JemIntroTileComponent extends DashBoardTileComponent{
-  title: string = 'Code Jems Intro Tile'
 }
