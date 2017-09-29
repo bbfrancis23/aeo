33f03555195f2c47653ae097c8886cba0cd2d3ec
Brian Francis <bbfrancis23@gmail.com>
2017-09-29 08:18:17 -0600

    Update dash-board component

--- src/app/dash-board/components/ts/dash-board-tile.ts
+++ src/app/dash-board/components/ts/dash-board-tile.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'dash-board-tile',
-  templateUrl: '../html/dash-board-tile.html'
+  templateUrl: ''
 })
 export class DashBoardTileComponent {
   show = true;
--- src/app/dash-board/components/ts/dash-board.ts
+++ src/app/dash-board/components/ts/dash-board.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'dash-board',
-  templateUrl: '../html/dash-board.html',
-  styleUrls: ['../css/dash-board.css']
+  template: '',
 })
 export class DashBoardComponent {
 
