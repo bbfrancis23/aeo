c29d07975c9fab0accbf5ec4b4ed8ed099b7ea0a
Brian Francis <bbfrancis23@gmail.com>
2017-09-28 19:14:52 -0600

    Update item controls

--- src/app/dash-board/components/ts/item-controls.ts
+++ src/app/dash-board/components/ts/item-controls.ts
@@ -3,19 +3,18 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'item-controls',
   template: `<div class="item-controls">
-                  <div class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown"></a>
-                    <div class="dropdown-menu dropdown-menu-right" >
-                     <a class="dropdown-item" (click)="deleteItemEvent.emit()" >Delete</a>
-                     <a class="dropdown-item" (click)="updateItemEvent.emit();">Update</a>
-                     <a class="dropdown-item" >Favorite</a>
-                   </div>
+                <div class="dropdown">
+                  <a class="dropdown-toggle" data-toggle="dropdown"></a>
+                  <div class="dropdown-menu dropdown-menu-right" >
+                   <a class="dropdown-item" (click)="deleteItemEvent.emit()" ><div class="material-icons drawer mi-sm">delete_forever</div> Delete</a>
+                   <a class="dropdown-item" (click)="updateItemEvent.emit();"><div class="material-icons drawer mi-sm">create</div> Update</a>
+                   <a class="dropdown-item" ><div class="material-icons drawer mi-sm">favorite</div> Favorite</a>
                  </div>
-                </div>`
+               </div>
+              </div>`
 })
 export class ItemControlsComponent {
-
-  //@Output() hideTileEvent = new EventEmitter();
+  
   @Output() deleteItemEvent = new EventEmitter();
   @Output() updateItemEvent = new EventEmitter();
 
