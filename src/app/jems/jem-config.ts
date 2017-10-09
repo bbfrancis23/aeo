c2f45fc753e1074939daccd741e4bf6bbcf27b62
Brian Francis <bbfrancis23@gmail.com>
2017-10-09 00:37:44 -0600

    Add jem-config.ts

---
+++ src/app/jems/jem-config.ts
@@ -0,0 +1,16 @@
+import { Config } from '../dash-board/config';
+
+export const JEM_CONFIG: Config = {
+  title: 'Code Jems',
+  name: 'jems',
+  intro: "Short-cut keys, Best Practices, How to and Mistakes. Code Jems,  it's all here",
+  img: "assets/img/code-jems.jpg",
+
+  fieldsRaw: [
+    { name: 'tech', values: ['Angular 4', 'CSS', 'Express', 'Git', 'HTML', 'JavaScript', 'Less', 'MongoDB', 'Mean Stack', 'NodeJS', 'TypeScript'] },
+    { name: 'type', values: ['Best Practices', 'How to', 'Mistakes', 'Short-Cut Keys', 'Style Guide'] }
+  ],
+  fields: []
+};
+
+/* copyright 2017 AEO All Rights Reserved. */
