95bcf82a842eda2c83847a39f0396680a10a3501
Brian Francis <bbfrancis23@gmail.com>
2017-10-08 19:00:18 -0600

    Tidy dash-board-service part duex

--- src/app/dash-board/dash-board.service.ts
+++ src/app/dash-board/dash-board.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core' ;
+import { Injectable } from '@angular/core';
 
 import { Headers, Http } from '@angular/http';
 
@@ -9,21 +9,32 @@ import { Config } from './config';
 @Injectable()
 export class DashBoardService {
 
-    config: Config;
+  config: Config;
 
-    private itemsSource = new BehaviorSubject<Object[]>([]);
-    private filteredItemsSource = new BehaviorSubject<Object[]>([]);
-    private selectedItemSource = new BehaviorSubject<Object>({})
+  private itemsSource = new BehaviorSubject<Object[]>([]);
+  private filteredItemsSource = new BehaviorSubject<Object[]>([]);
+  private selectedItemSource = new BehaviorSubject<Object>({})
 
-    currentItems = this.itemsSource.asObservable();
+  currentItems = this.itemsSource.asObservable();
+  currentFilteredItems = this.filteredItemsSource.asObservable();
+  currentSelectedItem = this.selectedItemSource.asObservable();
 
-    constructor(private httpG: Http){} // todo see if we can get rid of the httpG
+  constructor(private httpG: Http) { } // todo see if we can get rid of the httpG
 
-    // todo see if there is a way of getting rid of observable => Promise => observable;
-    refresh(){
-      let items = this.httpG.get(`api/${this.config.name}`).toPromise().then(response => response.json().data);
-      items.then(items=> this.changeItems(items));
-    }
+  // todo see if there is a way of getting rid of observable => Promise => observable;
+  // todo add filtering in here
+  refresh() {
+    let items = this.httpG.get(`api/${this.config.name}`).toPromise().then(response => response.json().data);
+    items.then(items => {
+      this.changeItems(items);
+      this.changeFilteredItems(items);
+      this.currentItems.subscribe(items => {
+        this.changeSelectedItem(items[0]);
+      });
+    });
+  }
 
-    changeItems( items: any[] ){ this.itemsSource.next( items ); }
+  changeItems(items: Object[]) { this.itemsSource.next(items) }
+  changeFilteredItems(filteredItems: Object[]) { this.filteredItemsSource.next(filteredItems) }
+  changeSelectedItem(selectedItem: Object) { this.selectedItemSource.next(selectedItem) }
 }
