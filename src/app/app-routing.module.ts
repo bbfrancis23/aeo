262ae88aa544749e38835ea9ccd16536998d85aa
Brian Francis <bbfrancis23@gmail.com>
2017-12-18 12:48:02 -0700

    quick fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,15 @@ import { JemMilieuComponent } from './jems/jem-milieu.component';
 import { AppContentComponent} from './app.content.component';
 
 const routes: Routes = [
-  //{ path: 'account', component: AccountMilieuComponent },
   { path: 'code-jems', component: JemMilieuComponent },
   { path: 'code-jems/tech/:tech', component: JemMilieuComponent },
   { path: 'code-jems/type/:type', component: JemMilieuComponent },
   { path: 'code-jems/tech/:tech/type/:type', component: JemMilieuComponent },
-  //{ path: 'jody', component: MagicHandsHealingComponent},
-  //{ path: 'real-estate-agent', component: RealEstateAgentMilieuComponent },
   { path: '', component: AppContentComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  //exports: [RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
