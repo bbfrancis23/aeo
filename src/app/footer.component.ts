e1a018f033e84cfdae77d7e7fe749c48cbe32de6
Brian Francis <bbfrancis23@gmail.com>
2017-12-31 17:24:48 -0700

    Footer perfection

--- src/app/footer.component.ts
+++ src/app/footer.component.ts
@@ -6,11 +6,11 @@ import { Component } from '@angular/core';
     <footer class="fluid-container">
       <div class="row">
         <div class="col-lg-12">
-          <a href="https://angular.io" target="_blank"><img class="angular" src="assets/img/angular.png" title="Angular" ></a>
-          <a href="https://www.mongodb.com/" target="_blank"><img class="mongo" src="assets/img/mongo.png" title="MongoDB" ></a>
-          <a href="https://nodejs.org" target="_blank"><img class="node" src="assets/img/nodejs.png" title="NodeJS" style=""></a>
-          <a href="https://getbootstrap.com" target="_blank"><img class="bootstrap" src="assets/img/bootstrap.png" title="Bootstrap" style=""></a>
-          <a href="https://www.digitalocean.com" target="_blank"><img class="digitalocean" src="assets/img/digitalocean.png" title="DigitalOcean" style=""></a>
+          <a href="https://angular.io" target="_blank"><img src="assets/img/angular.png" title="Angular" ></a>
+          <a href="https://www.mongodb.com/" target="_blank"><img src="assets/img/mongo.png" title="MongoDB" ></a>
+          <a href="https://nodejs.org" target="_blank"><img src="assets/img/nodejs.png" title="NodeJS" style=""></a>
+          <a href="https://getbootstrap.com" target="_blank"><img src="assets/img/bootstrap.png" title="Bootstrap" style=""></a>
+          <a href="https://www.digitalocean.com" target="_blank"><img src="assets/img/digitalocean.png" title="DigitalOcean" style=""></a>
         </div>
       </div>
       <div class="row">
