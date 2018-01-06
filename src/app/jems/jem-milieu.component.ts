88867b0f66453bf06d14fe6480d125cab2a99c40
Brian Francis <bbfrancis23@gmail.com>
2018-01-06 09:41:36 -0700

    Audit jem-milieu-Component

--- src/app/jems/jem-milieu.component.ts
+++ src/app/jems/jem-milieu.component.ts
@@ -1,24 +1,64 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 
-import { Milieu } from '../milieu/core';
+import { Milieu, FilterVueComponent, IntroVueComponent } from '../milieu/core';
+import { MilieuModalComponent} from '../milieu/modals';
 import { MilieuService } from '../milieu/milieu.service';
-import { FilterVueComponent } from '../milieu/core';
-import { IntroVueComponent } from '../milieu/core';
 
-
-import { JEM_CONFIG } from './jem-config';
-import { Jem } from './jem';
 import { JemListVueComponent } from './core';
 import { JemService } from './jem.service';
 import { ManageJemComponent } from './manage';
-import { MilieuModalComponent} from '../milieu/modals';
 
 'use strict';
 
 @Component({
   selector: 'jem-milieu',
-  templateUrl: './jem-milieu.component.html',
+  template: `
+    <div (click)="clickCheck($event)">
+      <div class="sticky-top container-fluid">
+        <div class="row" >
+          <div class="col-6 headline" ><h1>{{jemService.pageTitle}}</h1></div>
+          <div class="col-6" >
+
+            <!-- <div class="ml-2 float-right  btn-group">
+                <button class="btn material-icons"  title="Jem Filter View" (click)="accountServices.modalMode = true"  >filter_list</button>
+                <button class="btn material-icons"  title="Add Jem View">add</button>
+            </div> -->
+
+            <div class="ml-2 float-right d-none d-md-block d-lg-block" *ngIf="jemService.authenticated === true">
+              <button class="btn btn-outline-secondary material-icons" title="Dash Board" [ngClass]="{'active': jemService.dashBoard}"  (click)="toggleDashBoard()">dashboard</button>
+            </div>
+            <div class="btn-group float-right d-none d-md-block d-lg-block" *ngIf="jemService.dashBoard" >
+              <button class="btn material-icons" (click)="introVue.show = !introVue.show" [ngClass]="{'active': introVue.show}"  title="Jem Info View">info_outline</button>
+              <button class="btn material-icons" (click)="toggleFilterVue();" [ngClass]="{'active':filterVue.show}"  title="Jem Filter View" >filter_list</button>
+              <button class="btn material-icons" (click)="toggleListVue();" [ngClass]="{'active': listVue.show}"  title="Jem List View">list</button>
+              <button class="btn material-icons" (click)="addVue.show = !addVue.show" [ngClass]="{'active': addVue.show}"  title="Add Jem View">add</button>
+              <button class="btn material-icons" (click)="updateVue.show = !updateVue.show" [ngClass]="{'active': updateVue.show}"  title="Edit Jem View">create</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="milieu-content " [ngClass]=" (isColumnVisible(0) && isColumnVisible(1) && isColumnVisible(2)) ? 'container-fluid' : 'container-classic' " >
+        <div class="row">
+          <div [ngClass]=" (isColumnVisible(0) && isColumnVisible(1) && isColumnVisible(2)) ? 'col-lg-2' : 'col-lg-3' ">
+            <intro-vue id="jem-intro-vue" [milieuService]="jemService"></intro-vue>
+            <filter-vue  class="jem-filter-vue" [milieuService]="jemService"></filter-vue>
+          </div>
+          <div [ngClass]="isColumnVisible(0) ? (   (isColumnVisible(0) && isColumnVisible(1) && isColumnVisible(2)) ? 'col-lg-10' : 'col-lg-9' ) : 'col-lg-12'">
+            <div class="row">
+              <div  [ngClass]=" isColumnVisible(2) ? 'col-lg-6' : 'col-lg-12'">
+                <jem-list-vue  (selectItemEvent)="selectJem($event)" [jemService]="jemService"></jem-list-vue>
+              </div>
+              <div  [ngClass]=" isColumnVisible(1) ? 'col-lg-6' : 'col-lg-12'">
+                <manage-jem [jemService]="jemService" [manageType]="'Add'"  #manageAdd></manage-jem>
+                <manage-jem [jemService]="jemService" [manageType]="'Update'" #manageUpdate></manage-jem>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <milieu-modal id="filter-modal"><filter-vue  class="jem-filter-vue" [milieuService]="jemService"></filter-vue></milieu-modal>`,
   styles: [`
 
     .container-classic{
@@ -34,53 +74,26 @@ import { MilieuModalComponent} from '../milieu/modals';
 })
 export class JemMilieuComponent extends Milieu implements OnInit {
 
-  jems: Jem[];
-  selectedJem: Jem;
-
-  state = 'active';
-
-  toggle() {
-    this.state = this.state === 'active' ? 'inactive' : 'active';
-  }
-
   @ViewChild(JemListVueComponent) listVue;
   @ViewChild('manageAdd') addVue: ManageJemComponent;
   @ViewChild('manageUpdate') updateVue: ManageJemComponent;
   @ViewChild(IntroVueComponent) introVue;
   @ViewChild(FilterVueComponent) filterVue;
 
-
   @ViewChild(MilieuModalComponent) accountServices: MilieuModalComponent;
 
   constructor(protected route: ActivatedRoute, protected jemService: JemService) {
     super(route, jemService);
     jemService.routeConfig(route);
-    jemService.refresh();
   }
 
 
   ngOnInit() {
-
-    console.log(this.jemService.tabletMode);
-
     if (!this.jemService.dashBoard) {
       this.addVue.show = false;
       this.updateVue.show = false;
     }
-
-    //if(this.jemService.tabletMode){
-    //  this.columns = [ [this.introVue], [this.listVue], [this.addVue,this.updateVue]];
-
-    //  this.filterVue.show = false;
-    //  this.filterVue.modalOnlyMode = true;
-
-    //}else{
-      this.columns = [ [this.introVue, this.filterVue], [this.listVue], [this.addVue,this.updateVue]];
-    //}
-
-
-    console.log(this.jemService);
-
+    this.columns = [ [this.introVue, this.filterVue], [this.listVue], [this.addVue,this.updateVue]];
   }
 
   toggleDashBoard() {
@@ -88,7 +101,6 @@ export class JemMilieuComponent extends Milieu implements OnInit {
     this.jemService.dashBoard = !this.jemService.dashBoard;
 
     if (this.jemService.dashBoard) {
-      //this.updateVue.show = true;
       this.addVue.show = true;
     } else {
       this.updateVue.show = false;
@@ -98,18 +110,12 @@ export class JemMilieuComponent extends Milieu implements OnInit {
   }
 
   toggleFilterVue(): void {
-
-    //if(this.jemService.tabletMode){
-    //  this.filterVue.modalChild.modalMode = true;
-    //  this.filterVue.show =  true;
-    //}else{
       if (this.filterVue.show === true) {
         this.filterVue.show = false
       } else {
         this.filterVue.show = true;
         this.listVue.show = true;
       }
-    //}
   }
 
   toggleListVue(): void {
@@ -122,12 +128,10 @@ export class JemMilieuComponent extends Milieu implements OnInit {
   }
 
   clickCheck(e){
-    //console.log(e.target.className);
     if(e.target.className.search('update') > -1){
       this.updateVue.show = true;
       this.updateVue.modalChild.modalMode = true;
     }
   }
-
 }
 /* Copyright AEO all rights reserved */
