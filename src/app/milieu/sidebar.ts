d4038c911bab1be69a07413d03909e165e451ff0
Brian Francis <bbfrancis23@gmail.com>
2018-02-01 15:26:06 -0700

    Add sidebar

--- src/app/milieu/sidebar.ts
+++ src/app/milieu/sidebar.ts
@@ -14,42 +14,7 @@ import { MilieuService} from './data';
         <ng-content></ng-content>
       </div>
     </div>
-  </div>
-
-
-    `,
-  styles: [`
-
-    .sidebar{
-      border: 1px solid gray;
-      position: -webkit-sticky;
-      position: sticky;
-      top: 122px; /* Height of navbar */
-      height: calc(100vh - 122px);
-      overflow-x: hidden;
-      overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
-    }
-
-    .drawer{
-      left: 0; top: 0;
-      width: 100%; height: 100%;
-      display: block;
-      position: fixed;
-      z-index: 3;
-      overflow: auto;
-      background-color: rgba(0,0,0,0.5);
-    }
-
-    .modal-vue-content{
-      width: 300px;
-      position: relative;
-      top: 122px; /* Height of navbar */
-      height: calc(100vh - 122px);
-      overflow-x: hidden;
-      overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
-    }
-
-    `]
+  </div>`,
 })
 export class MilieuSideBarComponent extends MilieuVue {
 
@@ -60,14 +25,7 @@ export class MilieuSideBarComponent extends MilieuVue {
 
   constructor(public milieuService: MilieuService){ super(milieuService);}
 
-  sidebarClick(e) {
-
-    if(e.target.className.search(/modal-mode/) > -1  || e.target.className.search(/close-modal/) > -1){
-
-      this.show = false;
-
-    }
-  }
+  sidebarClick(e) { if(e.target.className.search(/modal-mode/) > -1  || e.target.className.search(/close-modal/) > -1) this.show = false }
 }
 
 @Component({
